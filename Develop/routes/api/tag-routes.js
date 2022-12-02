939fb2c038bfeea73ce4cfb0ec456762e18ec4d8
Kyle Nguyen <kylenguyen@kyles-air.mynetworksettings.com>
2022-12-01 19:01:30 -0500

    set up routes

---
+++ Develop/routes/api/tag-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  Tag.findAll({include: [Product]}).then(category => {res.json(category)})
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  Tag.findOne({where:{id:req.params.id}},{include: [Product]}).then(category => {res.json(category)})
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+  Tag.create(req.body).then(category => {res.json(category)})
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update(req.body, {where: {id:req.params.id}}).then(category => {res.json(category)})
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  Tag.destroy({where: {id:req.params.id}}).then(category => {res.json(category)})
+});
+
+module.exports = router;
